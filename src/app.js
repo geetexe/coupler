0b012e91ddba7e1ab3f4bb89d4afb09ec8afd59d
Geet Trivedi <geettrivedi@Geets-MacBook-Pro.local>
2024-12-28 11:57:29 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.use('/hello', (req, res) => res.send("Hello world!"));
+
+app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
